6da40640c8ffe8b78d63a214861580f1a42a97d9
wejoey <wejoey@gmail.com>
2025-04-12 14:01:46 -0400

    Remove macOS temporarely

--- fish/conf.d/aliases.fish
+++ fish/conf.d/aliases.fish
@@ -1,9 +1,11 @@
-alias ll 'eza --long --all --time-style=long-iso --no-user --classify'
+alias ll 'exa --long --all --time-style=long-iso --no-user --classify'
+alias llmacos 'eza --long --all --time-style=long-iso --no-user --classify'
 alias python python3
 alias vim nvim
 
 alias gitpfwl 'git push --force-with-lease'
 alias gitca 'git commit --amend'
-alias gitccb 'git branch --show-current | pbcopy'
+alias gitccb 'git branch --show-current | xclip -selection clipboard -rmlastnl'
+alias gitccbmacos 'git branch --show-current | pbcopy'
 alias gitlog 'git log --oneline --graph --all --decorate'
 alias gitdiff 'git difftool -y'
