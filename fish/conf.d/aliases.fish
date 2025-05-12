c34c67ec3648aeb24dc2b5640d08bca479059f9d
Joey Lessard <jlessard@coveo.com>
2025-05-12 10:58:09 -0400

    Swap fish aliases to macos

--- fish/conf.d/aliases.fish
+++ fish/conf.d/aliases.fish
@@ -1,12 +1,12 @@
-alias ll 'exa --long --all --time-style=long-iso --no-user --classify'
-alias llmacos 'eza --long --all --time-style=long-iso --no-user --classify'
+alias lllinux 'exa --long --all --time-style=long-iso --no-user --classify'
+alias ll 'eza --long --all --time-style=long-iso --no-user --classify'
 alias python python3
 alias vim nvim
 
 alias gitpfwl 'git push --force-with-lease'
 alias gitca 'git commit --amend'
-alias gitccb 'git branch --show-current | xclip -selection clipboard -rmlastnl'
-alias gitccbmacos 'git branch --show-current | pbcopy'
+alias gitccblinux 'git branch --show-current | xclip -selection clipboard -rmlastnl'
+alias gitccb 'git branch --show-current | pbcopy'
 alias gitlog 'git log --oneline --graph --all --decorate'
 alias gitdiff 'git difftool -y'
 alias gitst 'git status -s'
